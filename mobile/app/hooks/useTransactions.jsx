73c11b747ad2345a2ce8ca671a12b097ee31b0e1
gulcin <gulcin_375@hotmail.com>
2025-10-10 13:16:51 +0200

    chore: move the API_URL to api.js file

--- mobile/app/hooks/useTransactions.jsx
+++ mobile/app/hooks/useTransactions.jsx
@@ -1,7 +1,6 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
-
-const API_URL = "http://localhost:5001/api/transactions";
+import { API_URL } from "../../constants/api";
 
 export const useTransactions = (userId) => {
   const [transactions, setTransaction] = useState([]);
---
+++ mobile/constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://wallet-api-n6zg.onrender.com/api/transactions";
